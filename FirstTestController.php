38a38ab81a08836a86b1ea7526bd235235e5b4ce
zhulang <zhulang@nxhjzddata.com>
2018-06-27 11:20:53 +0800

    修改和新增一个文件

---
+++ FirstTestController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @Time: [HJZD] (C)2018/06/27/11:00
+ * This is not a free software, without any authorization is not allowed to use and spread.
+ *
+ * @Id  : FirstTest.php by zhulang(zhulang@nxhjzddata.com)
+ */
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # firstTestDemo
 this xiaozhu firstDemo in gitHub
 I want  use this demo made me know git command
+11
